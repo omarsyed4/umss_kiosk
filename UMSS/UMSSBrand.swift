ac13ccb51a7aa1780bd697dd0a2297e56660f3ca
Omar Syed <omarsyed0504@gmail.com>
2025-02-06 23:20:39 -0500

    Added New PDF, changed style of questioning

---
+++ UMSS/UMSSBrand.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+struct UMSSBrand {
+    // Approximate brand colors inferred from your logo
+    static let gold = Color(red: 0.76, green: 0.58, blue: 0.35)
+    static let navy = Color(red: 0.18, green: 0.24, blue: 0.34)
+}
