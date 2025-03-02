31f12bd0efdcf856aa24ee127741022636cbd782
Omar Syed <omarsyed0504@gmail.com>
2025-03-02 17:12:42 -0500

    Update UserInterfaceState to reflect recent changes in user settings

--- UMSS/ContentView.swift
+++ UMSS/ContentView.swift
@@ -13,7 +13,7 @@ import FirebaseFirestore
 import Foundation
 
 struct ContentView: View {
-    // Patient flow pages (dashboard is separate)
+    // MARK: - Patient Flow Steps (dashboard is separate)
     enum PatientFlowStep: String, CaseIterable, Identifiable {
         case basicInfo = "Basic Info"
         case demographics = "Demographics"
@@ -24,27 +24,69 @@ struct ContentView: View {
     }
     
     // MARK: - State Variables
-    // This flag determines whether we are in the patient flow (which shows a sidebar) or on the dashboard.
+    // Flag for showing patient flow vs. dashboard.
     @State private var inPatientFlow: Bool = false
+    // The currently selected step in the patient flow.
     @State private var selectedFlowStep: PatientFlowStep = .basicInfo
     
     @StateObject private var viewModel = PatientModelViewModel()
     @StateObject private var appointmentVM = AppointmentViewModel()
     @StateObject private var officeViewModel = OfficeViewModel()
     
-    // PDF and upload states
+    // PDF and upload states.
     @State private var showPDFPreview = false
     @State private var isGeneratingPDF = false
     @State private var pdfDocument: PDFDocument?
     @State private var accessToken: String?
     @State private var uploadStatus: String = ""
     
-    // Additional states
+    // Additional states.
     @State private var isAddressPickerPresented = false
     @State private var isLoadingPatientData = false
     
-    // Folder ID for Drive uploads
+    // Folder ID for Drive uploads.
     let folderID = "16b3ZeFMpHft5yN8zGguhABeNjgrTa6Mu"
+    
+    // MARK: - Computed Validation Properties
+    private var isBasicInfoComplete: Bool {
+        let model = viewModel.patientModel
+        return !model.email.trimmingCharacters(in: .whitespaces).isEmpty &&
+               !model.firstName.trimmingCharacters(in: .whitespaces).isEmpty &&
+               !model.lastName.trimmingCharacters(in: .whitespaces).isEmpty &&
+               !model.dob.trimmingCharacters(in: .whitespaces).isEmpty &&
+               !model.age.trimmingCharacters(in: .whitespaces).isEmpty &&
+               !model.phone.trimmingCharacters(in: .whitespaces).isEmpty &&
+               !model.reasonForVisit.trimmingCharacters(in: .whitespaces).isEmpty
+    }
+    
+    private var isDemographicsComplete: Bool {
+        let model = viewModel.patientModel
+        return !model.selectedGender.trimmingCharacters(in: .whitespaces).isEmpty &&
+               !model.selectedRace.trimmingCharacters(in: .whitespaces).isEmpty &&
+               !model.selectedMaritalStatus.trimmingCharacters(in: .whitespaces).isEmpty &&
+               !model.selectedEthnicity.trimmingCharacters(in: .whitespaces).isEmpty &&
+               !model.selectedIncome.trimmingCharacters(in: .whitespaces).isEmpty &&
+               !model.address.trimmingCharacters(in: .whitespaces).isEmpty
+    }
+    
+    private var isSignatureComplete: Bool {
+        return viewModel.patientModel.signatureImage != nil
+    }
+    
+    // The steps that the user is allowed to navigate to.
+    private var allowedSteps: [PatientFlowStep] {
+        var steps: [PatientFlowStep] = [.basicInfo]
+        if isBasicInfoComplete {
+            steps.append(.demographics)
+            if isDemographicsComplete {
+                steps.append(.signature)
+                if isSignatureComplete {
+                    steps.append(.thankYou)
+                }
+            }
+        }
+        return steps
+    }
     
     var body: some View {
         ZStack {
@@ -62,26 +104,86 @@ struct ContentView: View {
                     }
                 )
             } else {
-                // Patient Flow – show a NavigationView with a sidebar for the patient flow steps.
+                // Patient Flow: NavigationView with a sidebar.
                 NavigationView {
-                    List(PatientFlowStep.allCases, id: \.self) { step in
-                        Button(action: { selectedFlowStep = step }) {
-                            Text(step.rawValue)
+                    // Sidebar with icons, step checks, and a Cancel button.
+                    VStack(alignment: .leading) {
+                        List(PatientFlowStep.allCases, id: \.self) { step in
+                            HStack {
+                                // Icon for each step.
+                                Group {
+                                    switch step {
+                                    case .basicInfo:
+                                        Image(systemName: "person.fill")
+                                    case .demographics:
+                                        Image(systemName: "info.circle.fill")
+                                    case .signature:
+                                        Image(systemName: "pencil.circle.fill")
+                                    case .thankYou:
+                                        Image(systemName: "checkmark.seal.fill")
+                                    }
+                                }
+                                .frame(width: 24)
+                                
+                                Text(step.rawValue)
+                                
+                                Spacer()
+                                // Show a checkmark if the step is complete.
+                                if (step == .basicInfo && isBasicInfoComplete) ||
+                                   (step == .demographics && isDemographicsComplete) ||
+                                   (step == .signature && isSignatureComplete) {
+                                    Image(systemName: "checkmark")
+                                        .foregroundColor(.green)
+                                }
+                            }
+                            .contentShape(Rectangle())
+                            .onTapGesture {
+                                // Only allow navigating to a step if it is allowed (or if it’s a previous step).
+                                let allSteps = PatientFlowStep.allCases
+                                if let tappedIndex = allSteps.firstIndex(of: step),
+                                   let currentIndex = allSteps.firstIndex(of: selectedFlowStep),
+                                   tappedIndex <= currentIndex || allowedSteps.contains(step) {
+                                    selectedFlowStep = step
+                                }
+                            }
+                            .disabled(!(allowedSteps.contains(step) || step == .basicInfo))
+                        }
+                        Spacer()
+                        // Cancel button to reset patient data and return to dashboard.
+                        Button(action: {
+                            viewModel.resetPatientData()
+                            inPatientFlow = false
+                        }) {
+                            HStack {
+                                Image(systemName: "xmark.circle.fill")
+                                Text("Cancel")
+                            }
+                            .foregroundColor(.red)
+                            .padding()
                         }
                     }
+                    .frame(minWidth: 250)
                     .listStyle(SidebarListStyle())
                     .navigationTitle("Patient Flow")
                     
-                    // Main content area
+                    // Main content area.
                     content(for: selectedFlowStep)
+//                        .toolbar {
+//                            ToolbarItem(placement: .navigationBarLeading) {
+//                                Button("Dashboard") {
+//                                    inPatientFlow = false
+//                                }
+//                            }
+//                        }
                 }
-                .navigationViewStyle(DoubleColumnNavigationViewStyle())            }
+                .navigationViewStyle(DoubleColumnNavigationViewStyle())
+            }
         }
         .onAppear {
             officeViewModel.fetchOffices()
             appointmentVM.checkForTodayClinic()
         }
-        // PDF Preview Sheet
+        // PDF Preview Sheet.
         .sheet(isPresented: $showPDFPreview) {
             if let pdfDocument = pdfDocument {
                 PDFPreviewView(pdfDocument: pdfDocument, onUpload: {
@@ -92,7 +194,7 @@ struct ContentView: View {
                 Text("Error generating PDF.")
             }
         }
-        // Address Picker Sheet
+        // Address Picker Sheet.
         .sheet(isPresented: $isAddressPickerPresented) {
             GoogleAddressAutocompleteView(
                 rawAddress: $viewModel.patientModel.rawAddress,
@@ -107,64 +209,94 @@ struct ContentView: View {
         }
     }
     
-    // Returns the view for each patient flow step.
+    // MARK: - Main Content for Each Step
     @ViewBuilder
     private func content(for step: PatientFlowStep) -> some View {
         switch step {
         case .basicInfo:
-            BasicInfoStepView(
-                email: $viewModel.patientModel.email,
-                firstName: $viewModel.patientModel.firstName,
-                lastName: $viewModel.patientModel.lastName,
-                dob: $viewModel.patientModel.dob,
-                age: $viewModel.patientModel.age,
-                phone: $viewModel.patientModel.phone,
-                reasonForVisit: $viewModel.patientModel.reasonForVisit,
-                isExistingPatient: $viewModel.patientModel.isExistingPatient,
-                initialDate: viewModel.patientModel.dateOfBirth
-            )
+            VStack {
+                BasicInfoStepView(
+                    email: $viewModel.patientModel.email,
+                    firstName: $viewModel.patientModel.firstName,
+                    lastName: $viewModel.patientModel.lastName,
+                    dob: $viewModel.patientModel.dob,
+                    age: $viewModel.patientModel.age,
+                    phone: $viewModel.patientModel.phone,
+                    reasonForVisit: $viewModel.patientModel.reasonForVisit,
+                    isExistingPatient: $viewModel.patientModel.isExistingPatient,
+                    initialDate: viewModel.patientModel.dateOfBirth
+                )
+                nextButton(currentStep: .basicInfo, nextStep: .demographics, isComplete: isBasicInfoComplete)
+            }
         case .demographics:
-            DemographicsStep(
-                selectedGender: $viewModel.patientModel.selectedGender,
-                selectedRace: $viewModel.patientModel.selectedRace,
-                selectedMaritalStatus: $viewModel.patientModel.selectedMaritalStatus,
-                selectedEthnicity: $viewModel.patientModel.selectedEthnicity,
-                selectedIncome: $viewModel.patientModel.selectedIncome,
-                isMale: $viewModel.patientModel.isMale,
-                isFemale: $viewModel.patientModel.isFemale,
-                isWhite: $viewModel.patientModel.isWhite,
-                isBlack: $viewModel.patientModel.isBlack,
-                isAsian: $viewModel.patientModel.isAsian,
-                isAmIndian: $viewModel.patientModel.isAmIndian,
-                isHispanic: $viewModel.patientModel.isHispanic,
-                isNonHispanic: $viewModel.patientModel.isNonHispanic,
-                isSingle: $viewModel.patientModel.isSingle,
-                isMarried: $viewModel.patientModel.isMarried,
-                isDivorced: $viewModel.patientModel.isDivorced,
-                isWidowed: $viewModel.patientModel.isWidowed,
-                selectedFamilySize: $viewModel.patientModel.selectedFamilySize,
-                selectedIncomeThreshold: $viewModel.patientModel.selectedIncomeThreshold,
-                fullAddress: $viewModel.patientModel.rawAddress,
-                streetAddress: $viewModel.patientModel.address,
-                city: $viewModel.patientModel.city,
-                state: $viewModel.patientModel.state,
-                zip: $viewModel.patientModel.zip,
-                cityStateZip: $viewModel.patientModel.cityStateZip,
-                isPickerPresented: $isAddressPickerPresented
-            )
+            VStack {
+                DemographicsStep(
+                    selectedGender: $viewModel.patientModel.selectedGender,
+                    selectedRace: $viewModel.patientModel.selectedRace,
+                    selectedMaritalStatus: $viewModel.patientModel.selectedMaritalStatus,
+                    selectedEthnicity: $viewModel.patientModel.selectedEthnicity,
+                    selectedIncome: $viewModel.patientModel.selectedIncome,
+                    isMale: $viewModel.patientModel.isMale,
+                    isFemale: $viewModel.patientModel.isFemale,
+                    isWhite: $viewModel.patientModel.isWhite,
+                    isBlack: $viewModel.patientModel.isBlack,
+                    isAsian: $viewModel.patientModel.isAsian,
+                    isAmIndian: $viewModel.patientModel.isAmIndian,
+                    isHispanic: $viewModel.patientModel.isHispanic,
+                    isNonHispanic: $viewModel.patientModel.isNonHispanic,
+                    isSingle: $viewModel.patientModel.isSingle,
+                    isMarried: $viewModel.patientModel.isMarried,
+                    isDivorced: $viewModel.patientModel.isDivorced,
+                    isWidowed: $viewModel.patientModel.isWidowed,
+                    selectedFamilySize: $viewModel.patientModel.selectedFamilySize,
+                    selectedIncomeThreshold: $viewModel.patientModel.selectedIncomeThreshold,
+                    fullAddress: $viewModel.patientModel.rawAddress,
+                    streetAddress: $viewModel.patientModel.address,
+                    city: $viewModel.patientModel.city,
+                    state: $viewModel.patientModel.state,
+                    zip: $viewModel.patientModel.zip,
+                    cityStateZip: $viewModel.patientModel.cityStateZip,
+                    isPickerPresented: $isAddressPickerPresented
+                )
+                nextButton(currentStep: .demographics, nextStep: .signature, isComplete: isDemographicsComplete)
+            }
         case .signature:
-            SignatureStep(signatureImage: $viewModel.patientModel.signatureImage)
-                .padding(.horizontal, 20)
+            VStack {
+                SignatureStep(signatureImage: $viewModel.patientModel.signatureImage)
+                    .padding(.horizontal, 20)
+                nextButton(currentStep: .signature, nextStep: .thankYou, isComplete: isSignatureComplete)
+            }
         case .thankYou:
-            // When finished, the Thank You view calls the onFinish callback to return to the dashboard.
-            ThankYouView(onFinish: { inPatientFlow = false },
-                         onPreviewPDF: { previewPDFAction() })
+            // Thank You view offers a Preview PDF button and a Return to Dashboard option.
+            ThankYouView(
+                onFinish: {
+                    viewModel.resetPatientData()
+                    inPatientFlow = false
+                },
+                onPreviewPDF: { previewPDFAction() }
+            )
         }
+    }
+    
+    // MARK: - Next Button Helper
+    private func nextButton(currentStep: PatientFlowStep, nextStep: PatientFlowStep, isComplete: Bool) -> some View {
+        Button(action: {
+            selectedFlowStep = nextStep
+        }) {
+            Text("Next")
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(isComplete ? UMSSBrand.gold : Color.gray)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+        }
+        .disabled(!isComplete)
+        .padding()
     }
     
     // MARK: - Appointment Handling
     private func handleAppointmentSelection(_ appointment: Appointment) {
-        // For booked appointments with existing patient data, load the patient info then jump to the signature step.
+        // For booked appointments with existing patient data, load and jump to signature.
         if appointment.booked == true && !appointment.patientId.isEmpty {
             isLoadingPatientData = true
             viewModel.patientModel.appointmentId = appointment.id
@@ -177,12 +309,12 @@ struct ContentView: View {
                             selectedFlowStep = .signature
                         }
                     } else {
-                        // Handle error – for example, show an alert.
+                        // Handle error – e.g., show an alert.
                     }
                 }
             }
         } else {
-            // For walk-ins or new appointments, reset and start at the basic info step.
+            // For walk-ins or new appointments, reset and start at basic info.
             viewModel.patientModel.appointmentId = appointment.id
             viewModel.resetPatientData()
             inPatientFlow = true
@@ -307,7 +439,7 @@ struct DashboardView: View {
 }
 
 struct ThankYouView: View {
-    // Added onPreviewPDF callback property.
+    // Callbacks for finishing the flow or previewing the PDF.
     let onFinish: () -> Void
     let onPreviewPDF: () -> Void
     
@@ -322,7 +454,6 @@ struct ThankYouView: View {
                 .font(.headline)
                 .multilineTextAlignment(.center)
                 .foregroundColor(.primary)
-            // New Preview PDF button
             Button("Preview PDF", action: onPreviewPDF)
                 .font(.headline)
                 .padding()
